fe0897e74b34a9deb4db4b90fa7805579ce11f47
Tyler Brown <tyler@sliceofnet.com>
2018-01-06 04:34:03 -0500

    Added enums for usage.

---
+++ app/entities/enums/SpecialEffect.scala
@@ -0,0 +1,9 @@
+package entities.enums
+
+class SpecialEffect {}
+
+object SpecialEffect extends Enumeration {
+  val Adapt, Battlecry, Charge, ChooseOne, Combo, Counter, Deathrattle, Discover, DivineShield,
+      Enrage, Freeze, Immune, Inspire, Lifesteal, Mega_Windfury, Overload, Poisonous, Quest,
+      Secret, Silence, Stealth, SpellDamage, Taunt, Windfury = Value
+}

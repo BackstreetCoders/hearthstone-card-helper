0246926eead75a639b1e6f66aaa92895098d93c8
Tyler Brown <tyler@sliceofnet.com>
2018-01-11 20:22:59 -0500

    Added archetype enum

---
+++ app/entities/enums/Archetype.scala
@@ -0,0 +1,10 @@
+package entities.enums
+
+class Archetype {}
+
+object Archetype extends Enumeration {
+  val Aggro, Control, Tempo, Fatigue, Combo, Miracle, Secret, Quest, Midrange,
+  Big, CThun, Discard, Dragon, Elemental, Evolve, Zoo, Hand, Murloc, Pirate,
+  Freeze, Jade, Ramp, Spell, Face, Deathrattle, Late, Handbuff, Buff, Highlander,
+  Silence, Cube
+}

1bfd1d5cbf965d01cd70f9fbc2c828e329642ffc
Calvin Do <blazezekrom@gmail.com>
2018-01-11 20:25:01 -0500

    Adding Deck Class.

---
+++ app/entities/Deck.scala
@@ -0,0 +1,15 @@
+package entities
+import enums._
+
+class Deck (
+  val name: String,
+  val cards: Array[Cards],
+  val hero_class: HeroClass,
+  val archetype: Archetype) {
+
+  def card_count: Int = { cards.length }
+  def dust_cost: Int = { cards.map(_dust_cost).sum }
+  def add_card(card: Card) = { cards :+ card }
+  def remove_card(card: Card) = { cards :- card } 
+
+}
